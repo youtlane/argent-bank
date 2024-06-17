a41beb81fb78fdd58aa6570fe21b6a96547ece2e
youtlane <youtlanehanane@gmail.com>
2024-06-17 22:41:50 +0100

    feat: create header component

---
+++ argent-bank-front/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+<nav class="main-nav">
+    <a class="main-nav-logo" href="./index.html">
+        <img
+            class="main-nav-logo-image"
+            src="./img/argentBankLogo.png"
+            alt="Argent Bank Logo"
+        />
+        <h1 class="sr-only">Argent Bank</h1>
+    </a>
+    <div>
+        <a class="main-nav-item" href="./sign-in.html">
+            <i class="fa fa-user-circle"></i>
+            Sign In
+        </a>
+    </div>
+</nav>
