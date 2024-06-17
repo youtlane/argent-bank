6a4eaf58d60364f4636467361c2f7ab54d30bbd0
youtlane <youtlanehanane@gmail.com>
2024-06-17 22:44:14 +0100

    fix: header component

--- argent-bank-front/src/components/Header/Header.jsx
+++ argent-bank-front/src/components/Header/Header.jsx
@@ -1,16 +1,28 @@
-<nav class="main-nav">
-    <a class="main-nav-logo" href="./index.html">
-        <img
-            class="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
-            alt="Argent Bank Logo"
-        />
-        <h1 class="sr-only">Argent Bank</h1>
-    </a>
-    <div>
-        <a class="main-nav-item" href="./sign-in.html">
-            <i class="fa fa-user-circle"></i>
-            Sign In
-        </a>
-    </div>
-</nav>
+
+
+
+import React from 'react';
+import './Header.css'; // Assuming you have corresponding CSS file for styling
+
+const Header = () => {
+    return (
+        <nav className="main-nav">
+            <a className="main-nav-logo" href="./index.html">
+                <img
+                    className="main-nav-logo-image"
+                    src="./img/argentBankLogo.png"
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </a>
+            <div>
+                <a className="main-nav-item" href="./sign-in.html">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
