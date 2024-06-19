2b288d7d7db773a728506166e7b87be142cd63a5
youtlane <youtlanehanane@gmail.com>
2024-06-19 10:31:25 +0100

    fix: header style and display logo

--- argent-bank-front/src/components/Header/Header.jsx
+++ argent-bank-front/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 // src/components/Header/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-
+import logo from '../../assets/argentBankLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
@@ -10,14 +11,14 @@ const Header = () => {
             <Link className="main-nav-logo" to="/">
                 <img
                     className="main-nav-logo-image"
-                    src="/assets/argentBankLogo.png"
+                    src={logo}
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
                 <Link className="main-nav-item" to="/sign-in">
-                    <i className="fa fa-user-circle"></i>
+                    <FontAwesomeIcon icon={faUserCircle} />
                     Sign In
                 </Link>
             </div>
