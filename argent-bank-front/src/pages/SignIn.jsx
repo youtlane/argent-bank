24fa904aab2c951f7cd6f7c70dc45e845de3bad0
youtlane <youtlanehanane@gmail.com>
2024-06-24 14:30:20 +0100

    feat: add react form hook

--- argent-bank-front/src/pages/SignIn.jsx
+++ argent-bank-front/src/pages/SignIn.jsx
@@ -1,32 +1,46 @@
 // src/pages/SignIn.js
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-
 const SignIn = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        // Handle form submission, e.g., send data to the server
+    };
+
     return (
         <main className="main bg-dark">
             <section className="sign-in-content">
                 <FontAwesomeIcon icon={faUserCircle} className="sign-in-icon" />
                 <h1>Sign In</h1>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="input-wrapper">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" />
+                        <input
+                            type="text"
+                            id="username"
+                            {...register('username', { required: 'Username is required' })}
+                        />
+                        {errors.username && <p className="error-message">{errors.username.message}</p>}
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input
+                            type="password"
+                            id="password"
+                            {...register('password', { required: 'Password is required' })}
+                        />
+                        {errors.password && <p className="error-message">{errors.password.message}</p>}
                     </div>
                     <div className="input-remember">
-                        <input type="checkbox" id="remember-me" />
+                        <input type="checkbox" id="remember-me" {...register('rememberMe')} />
                         <label htmlFor="remember-me">Remember me</label>
                     </div>
-                    {/* PLACEHOLDER DUE TO STATIC SITE */}
-                    <a href="./user.html" className="sign-in-button">Sign In</a>
-                    {/* SHOULD BE THE BUTTON BELOW */}
-                    {/* <button className="sign-in-button">Sign In</button> */}
+                    <button type="submit" className="sign-in-button">Sign In</button>
                 </form>
             </section>
         </main>
